941a07bafa88888d3d17e31a402373657f2fa3ad
Ross Hetherton <rosshetherton@live.com>
2021-12-07 15:37:25 +1100

    got the book creation function working, as well as the function to push it into an array without deleting the previous entries

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # library
+An application designed to receive and store entries of books with their title, pages and author and whether or not they've been read, and display them in a user interface.
+
+GOAL
+Aim here is to develop an interactive page that will have check boxes or something similar for whether a book is read or not, so that when a person has finished the book they can mark it as read, and change the Book.read part in the array. Will need a form for entering new books that will then store the books in the array upon confirming, and a button to remove from the library if they're no longer in posession of the book. 
--- app.js
+++ app.js
@@ -7,10 +7,19 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 let firstBook = new Book("Bill Murray's guide to ball play", "Bill Murray", 35, "read");
+let secondBook = new Book("Book test", "john dicky", 42, "not read");
+;
 
 Book.prototype.bookFile = function() {
-    console.log(`${this.title} by ${this.author}, ${this.pages} pages long, ${this.read}`);
+    return `${this.title} by ${this.author}, ${this.pages} pages long, ${this.read}`;
 };
-function addBook() {
-    
+function addBook(book) {
+    myLibrary.push( [{"Title: ":book.title},
+                 {"Author: ":book.author},
+                 {"Pages: ":book.pages},
+                 {"Read: ":book.read}]);
+    console.log("Book added!");
 }
+
+addBook(firstBook);
+
